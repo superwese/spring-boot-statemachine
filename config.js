5a2a3628ac91a64e01fe926854cce30ae121813f
andrethierbach <athierbach@testo.de>
2020-11-09 14:51:40 +0100

    initial import

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    integration: {
+        europe: {
+            baseUrl: 'https://sample-data.i.europe.food.saveris.net',
+            username: '',
+            password: ''
+        }
+    },
+    qs: {
+        europe: {
+            baseUrl: 'https://sample-data.qs.europe.food.saveris.net',
+            username: '',
+            password: ''
+        }
+    },
+    production: {
+        americas: {
+            baseUrl: 'https://sample-data.americas.food.saveris.net',
+            username: '',
+            password: ''
+        },
+        apac: {
+            baseUrl: 'https://sample-data.apac.food.saveris.net',
+            username: '',
+            password: ''
+        },
+        europe: {
+            baseUrl: 'https://sample-data.europe.food.saveris.net',
+            username: '',
+            password: ''
+        }
+    }
+};
--- migrate.js
+++ migrate.js
@@ -1,5 +1,55 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const config = require('./config');
+
+const tenantUuids = ['5098df93-20c1-4a28-a007-cd548521d30a'];
+const firstDayToMigrate = moment('2016-10-01', 'YYYY-MM-DD');
+const lastDayToMigrate = moment('2020-11-11', 'YYYY-MM-DD');
+
+async function triggerMigration(tenantUuid, startDate, endDate, stage, region) {
+
+  const authorization = `Basic ${base64.encode(config[stage][region].username + ":" + config[stage][region].password)}`;
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': authorization
+  };
+
+  const body = {
+    tenantUuid: tenantUuid,
+    startDate: startDate,
+    endDate: endDate
+  };
+
+  const response = await fetch(
+    `${config[stage][region].baseUrl}/rest/admin/sample-data/api/migration/tasks-execution-import/`,
+    {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(body)
+    }
+  );
+
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    console.log(`Status code: ${response.status} triggerMigration`);
+    throw new Error(`Status code: ${response.status} triggerMigration`);
+  }
+}
+
 (async function() {
 
-  console.log('Hello World!');
+  for (const tenantUuid of tenantUuids) {
+    let currentDate = firstDayToMigrate;
+
+    do {
+      console.log(`trigger migration for ${tenantUuid}, ${currentDate.format('YYYY-MM-DD')}, ${currentDate.format('YYYY-MM-DD')}`);
+
+      // await triggerMigration(tenantUuid, currentDate.format('YYYY-MM-DD'), currentDate.format('YYYY-MM-DD'), 'integration', 'europe');
+
+      currentDate.add(1, 'days');
+    } while (!currentDate.isAfter(lastDayToMigrate))
+
+  }
 
 })().catch(console.error);
