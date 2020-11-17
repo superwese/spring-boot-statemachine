b3652563d531d71c2f764fcdee6f2f8605b61cc7
wese <wese@wese.org>
2020-11-18 00:38:17 +0100

    test the RequestHandler

--- statemachine/functions/GetTaskexecutions/src/test/java/getTaskexecutions/AppTest.java
+++ statemachine/functions/GetTaskexecutions/src/test/java/getTaskexecutions/AppTest.java
@@ -1,5 +1,10 @@
 package getTaskexecutions;
 
+import com.amazonaws.serverless.proxy.internal.servlet.StreamLambdaHandler;
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import getTaskexecutions.controller.GetTaskExecutionsController;
 import getTaskexecutions.model.Request;
 import getTaskexecutions.model.Response;
@@ -7,19 +12,27 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class AppTest {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new Jdk8Module())
+            .registerModule(new JavaTimeModule());
+    private final static MockLambdaContext context = new MockLambdaContext();
+
     @Test
-    public void successfulResponse() {
-        GetTaskExecutionsController app = new GetTaskExecutionsController();
+    public void testController() {
+        GetTaskExecutionsController controller = new GetTaskExecutionsController();
 
 
         Request event = new Request();
@@ -28,11 +41,34 @@ public class AppTest {
         event.setEndDate(Instant.now().plus(10, ChronoUnit.HOURS));
         event.setTenantUuid(UUID.randomUUID());
 
-        ResponseEntity<Response> response = app.handleRequest(event);
+        ResponseEntity<Response> response = controller.handleRequest(event);
 
         assertTrue(response.getStatusCode() == HttpStatus.OK);
 
         assertThat(response.getBody().getPage(), is(13));
+
+    }
+
+    @Test
+    public void testApp() throws IOException {
+
+        Request request = new Request();
+        request.setTenantUuid(UUID.randomUUID());
+        request.setPage(5);
+        request.setStartDate(Instant.now());
+        request.setEndDate(Instant.now().plus(10, ChronoUnit.MINUTES));
+
+        InputStream inputStream = new ByteArrayInputStream(
+                objectMapper.writeValueAsBytes(request));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        StreamLambdaHandler handler = new StreamLambdaHandler();
+        handler.handleRequest(inputStream, outputStream, context);
+
+        Response response = objectMapper.readValue(outputStream.toByteArray(), Response.class);
+
+        assertEquals(request.getPage(), response.getPage());
+        assertTrue(response.isLast());
 
     }
 }
