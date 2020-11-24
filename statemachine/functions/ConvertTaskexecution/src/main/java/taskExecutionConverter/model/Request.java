e8de9d4f5ea0a4ea2beb5b855154c539c52c2f19
wese <wese@wese.org>
2020-11-24 11:17:03 +0100

    JSON Property is "uuid"

--- statemachine/functions/ConvertTaskexecution/src/main/java/taskExecutionConverter/model/Request.java
+++ statemachine/functions/ConvertTaskexecution/src/main/java/taskExecutionConverter/model/Request.java
@@ -8,18 +8,22 @@ a request has this format:
 
  */
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Request {
+    @JsonProperty("uuid")
     private UUID taskExecutionUuid;
-
+    @JsonGetter("uuid")
     public UUID getTaskExecutionUuid() {
         return taskExecutionUuid;
     }
-
+    @JsonSetter("uuid")
     public void setTaskExecutionUuid(UUID taskExecutionUuid) {
         this.taskExecutionUuid = taskExecutionUuid;
     }
